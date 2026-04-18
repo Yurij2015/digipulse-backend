e5af0a4e95148bf382f70d49d1124c5030debf00
Yurii M <mokruy9999@gmail.com>
2026-04-18 13:07:29 +0200

    feat: implement Filament admin access control via configurable email environment variable

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -15,10 +17,15 @@ use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password', 'first_name', 'last_name', 'google_id', 'google_nickname', 'google_avatar', 'telegram_chat_id', 'telegram_connection_token'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasApiTokens, HasFactory, HasRoles, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->email === config('app.admin_email');
+    }
 
     /**
      * Get the attributes that should be cast.
