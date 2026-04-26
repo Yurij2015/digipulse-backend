3e5ed31838fd5f62b5d65c45e113115f7345004e
Yurii M <mokruy9999@gmail.com>
2026-04-27 01:09:01 +0200

    chore: update CORS allowed origins to include environment variable and production URL

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000', 
+        'http://127.0.0.1:3000', 
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'https://pulse.digispace.pro'
+    ],
 
     'allowed_origins_patterns' => [],
 
