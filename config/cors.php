fcb77487bf2fd322668f024864d051ac33c55605
Yurii M <mokruy9999@gmail.com>
2026-04-27 01:14:56 +0200

    chore: normalize whitespace and formatting in CORS allowed origins list

--- config/cors.php
+++ config/cors.php
@@ -20,10 +20,10 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:3000', 
-        'http://127.0.0.1:3000', 
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
         env('FRONTEND_URL', 'http://localhost:3000'),
-        'https://pulse.digispace.pro'
+        'https://pulse.digispace.pro',
     ],
 
     'allowed_origins_patterns' => [],
