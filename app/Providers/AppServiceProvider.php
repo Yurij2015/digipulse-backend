b98effd89a7c4d2ae18f99afff5d17e058cd54e9
Yurii M <mokruy9999@gmail.com>
2026-05-02 16:28:31 +0200

    feat: implement rate limiting for authentication and password reset routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
@@ -27,6 +30,14 @@ class AppServiceProvider extends ServiceProvider
         if (! $this->app->environment('local')) {
             URL::forceScheme('https');
         }
+
+        RateLimiter::for('login', static function (Request $request) {
+            return Limit::perMinute(5)->by($request->ip());
+        });
+
+        RateLimiter::for('register', static function (Request $request) {
+            return Limit::perMinute(3)->by($request->ip());
+        });
 
         ResetPassword::createUrlUsing(static function ($user, string $token) {
             return rtrim(config('app.frontend_url'), '/').'/auth/reset-password?token='.$token.'&email='.urlencode($user->email);
