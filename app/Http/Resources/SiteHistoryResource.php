c89030b4efa97e417b288a8360e84bc2a99f5f4e
Yurii M <mokruy9999@gmail.com>
2026-05-03 14:50:07 +0200

    feat: include latest check results in site history response

--- app/Http/Resources/SiteHistoryResource.php
+++ app/Http/Resources/SiteHistoryResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\CheckResultResource;
 
 class SiteHistoryResource extends JsonResource
 {
@@ -16,6 +17,12 @@ class SiteHistoryResource extends JsonResource
         return [
             'stats' => $this->resource['stats'],
             'incidents' => CheckResultResource::collection($this->resource['incidents']),
+            'latest_results' => array_map(fn ($item) => [
+                'config_id' => $item['config_id'],
+                'type_name' => $item['type_name'],
+                'type_slug' => $item['type_slug'],
+                'result' => new CheckResultResource($item['result']),
+            ], $this->resource['latest_results'] ?? []),
         ];
     }
 }
