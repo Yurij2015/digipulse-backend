c39b6c8be9f69ff274d91b96a5bab4dcd35a8a69
Yurii M <mokruy9999@gmail.com>
2026-05-05 15:44:15 +0200

    feat: implement IP and email-based authorization for Telescope and enable it across all environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local') && class_exists(TelescopeApplicationServiceProvider::class)) {
+        if (class_exists(TelescopeApplicationServiceProvider::class)) {
             $this->app->register(TelescopeServiceProvider::class);
         }
     }
